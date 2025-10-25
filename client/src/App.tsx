f678c5979f9373df03f7cfc28c64505b757cc2c1
tarek-abdelhak <tareq.abdelhaq98@gmail.com>
2025-10-25 13:54:18 +0300

    setup the client-app inside client/ folder

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="bg-gray-200 text-center">
+            <h1 className="text-2xl font-bold">Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
